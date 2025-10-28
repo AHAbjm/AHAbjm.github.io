affff39593a688dd920eec96d2ffe475066d52ac
AHA <2352230452@qq.com>
2025-10-29 06:20:07 +0800

    Site updated: 2025-10-29 06:20:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/29/hello-world/","2025/10/29/大家好/"];function toRandomPost(){
+var posts=["2025/10/29/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
